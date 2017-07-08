091e7b233a4549f118be37e1119df7afb674986e
Robert Jacob <xperimental@solidproject.de>
2017-07-08 16:55:16 +0200

    Use bash and set fail conditions. Reformat files.

--- rename.sh
+++ rename.sh
@@ -1,23 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo errexit
 
 if [ $# -ne 2 -o ! -d "$2" ]; then
-  echo "usage: $0 <ident> <folder>"
-  echo "E.g. $0 0.5.0 bin/ar71xxx/"
-  exit 1
+    echo "usage: $0 <ident> <folder>"
+    echo "E.g. $0 0.5.0 bin/ar71xxx/"
+    exit 1
 fi
 
 ident="$1"
 path="$2"
 
 for path in $(find "$path" -iname "lede*"); do
-        dir="${path%/*}"
-        file="${path##*/}"
-        if [ "$file" != "${file/$ident/}" ]; then
-                echo "Already contains '$ident': $path"
-                continue
-        fi
-	new_file="$ident"`echo "$file" | sed -e "s/lede-[^-]*-[^-]*//"`
-	mv "$path" "$dir/$new_file"
+    dir="${path%/*}"
+    file="${path##*/}"
+    if [ "$file" != "${file/$ident/}" ]; then
+        echo "Already contains '$ident': $path"
+        continue
+    fi
+    new_file="$ident"`echo "$file" | sed -e "s/lede-[^-]*-[^-]*//"`
+    mv "$path" "$dir/$new_file"
 done
 
 exit 0
